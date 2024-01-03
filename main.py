3941c4a840d91dd68f04de2a3ad6e691329ba5c5
Irving Amaechi <106456075+LeoIvin@users.noreply.github.com>
2024-01-03 22:21:50 +0000

    commit2

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m")
+# Initialize the text generation pipeline
+generator = pipeline('text-generation', model='bigscience/Bloom7b1')
+
+# Define the prompt
+prompt = "What do you know about Nigeria"
+
+# Generate text
+output = generator(prompt, max_length=100)
+
+
+# Print the generated text
+print(output[0]['generated_text'])
+
+
+""" tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-560m")
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m") """
